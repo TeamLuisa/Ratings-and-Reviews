d401d08cad4319669aa51994e8ce3a2df3b2043e
Wisdom Ibole <wisdom.ibole@gmail.com>
2022-01-14 17:48:49 -0800

    Adds sorting by reported

--- helpers/middleware.js
+++ helpers/middleware.js
@@ -1,9 +1,62 @@
 // Handles GET requests to /reviews
 // Query params:
 //   page (default: 1), count (default: 5), sort (by 'newest', 'helpful', 'relevant'), product_id
+const db = require('../database/index');
 
-// 1. From DB 'product' Table, of interest are:
-//    col 1, "id": 1 -> "product": String(1 + 40343) in RETURNED
-//    col 2, "name": "Camo Onesie" -> Product name in Modal??
+module.exports.getReviews = async (req, res) => {
+  const productId = req.query.product_id; // required
+  const page = req.query.page || 1; // not required
+  const count = req.query.count || 5; // not required
+  const sort = req.query.sort || 'newest'; // not required
+  let sortBy = 'date DESC';
 
-// let product_id_adjustor = 40343;
+  if (sort === 'helpful') {
+    sortBy = 'helpfulness DESC';
+  }
+  if (sort === 'relevant') {
+    sortBy = 'helpfulness DESC, date DESC';
+  }
+
+  const data = {
+    product_id: productId,
+    page: page - 1,
+    count,
+  };
+
+  const getQuery1 = `SELECT
+  JSON_AGG(JSON_BUILD_OBJECT('review_id', id, 'rating', rating, 'summary', summary,
+  'recommend', recommend, 'response', response, 'body', body, 'date', TO_TIMESTAMP( date/1000 ),
+  'reviewer_name', reviewer_name, 'helpfulness', helpfulness) ORDER BY ${sortBy}) AS results FROM reviews
+  WHERE product_id = ${productId} AND reported = false GROUP BY product_id LIMIT ${page * count}`;
+
+  // const getQuery = `SELECT r.id AS review_id, r.rating, r.summary, r.recommend, r.response,
+  // r.body, TO_TIMESTAMP( r.date/1000 ) AS date, r.reviewer_name, r.helpfulness,
+  // JSON_AGG(JSON_BUILD_OBJECT('id', p.id, 'url', p.url)) AS photos FROM reviews r JOIN
+  // reviews_photos p ON p.review_id = r.id WHERE r.product_id = ${productId} GROUP BY
+  // (r.id, r. rating, r.summary, r.recommend, r.response, r.body, r.date, r.reviewer_name,
+  // r.helpfulness)`;
+
+  const queryOutput = await db.query(getQuery1);
+  const { results } = queryOutput[0];
+  const queries = [];
+
+  for (let i = 0; i < results.length; i += 1) {
+    results[i].photos = [];
+
+    const getPhotosQuery = `SELECT JSON_AGG(JSON_BUILD_OBJECT('id', p.id, 'url', p.url)) AS photos
+    FROM reviews r RIGHT JOIN reviews_photos p ON p.review_id = r.id
+    WHERE p.review_id = ${results[i].review_id} GROUP BY p.review_id`;
+
+    queries.push(getPhotosQuery);
+  }
+
+  const returnedURLs = await Promise.all(queries.map((getPhotosQuery) => db.query(getPhotosQuery)));
+  returnedURLs.map((url, i) => {
+    if (url.length > 0) {
+      results[i].photos = results[i].photos.concat(url[0].photos);
+    }
+  });
+
+  data.results = results;
+  res.send(data);
+};
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { getReviews } = require('../helpers/middleware');
 const db = require('../database/index');
 
 const app = express();
@@ -8,64 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 const port = 3300;
 
 // db.connect();
-// const productIdAdjustor = 40343;
 
 app.get('/reviews', async (req, res) => {
-  const productId = req.query.product_id; // required
-  const page = req.query.page || 1; // not required
-  const count = req.query.count || 5; // not required
-  const sort = req.query.sort || 'newest'; // not required
-  let sortBy = 'date DESC';
-
-  if (sort === 'helpful') {
-    sortBy = 'helpfulness DESC';
-  }
-  if (sort === 'relevant') {
-    sortBy = 'helpfulness DESC, date DESC';
-  }
-
-  const data = {
-    product_id: productId,
-    page: page - 1,
-    count,
-  };
-
-  const getQuery1 = `SELECT
-  JSON_AGG(JSON_BUILD_OBJECT('review_id', id, 'rating', rating, 'summary', summary,
-  'recommend', recommend, 'response', response, 'body', body, 'date', TO_TIMESTAMP( date/1000 ),
-  'reviewer_name', reviewer_name, 'helpfulness', helpfulness) ORDER BY ${sortBy}) AS results FROM reviews
-  WHERE product_id = ${productId} GROUP BY product_id LIMIT ${page * count}`;
-
-  // const getQuery = `SELECT r.id AS review_id, r.rating, r.summary, r.recommend, r.response,
-  // r.body, TO_TIMESTAMP( r.date/1000 ) AS date, r.reviewer_name, r.helpfulness,
-  // JSON_AGG(JSON_BUILD_OBJECT('id', p.id, 'url', p.url)) AS photos FROM reviews r JOIN
-  // reviews_photos p ON p.review_id = r.id WHERE r.product_id = ${productId} GROUP BY
-  // (r.id, r. rating, r.summary, r.recommend, r.response, r.body, r.date, r.reviewer_name,
-  // r.helpfulness)`;
-
-  const queryOutput = await db.query(getQuery1);
-  const { results } = queryOutput[0];
-  const queries = [];
-
-  for (let i = 0; i < results.length; i += 1) {
-    results[i].photos = [];
-
-    const getPhotosQuery = `SELECT JSON_AGG(JSON_BUILD_OBJECT('id', p.id, 'url', p.url)) AS photos
-    FROM reviews r RIGHT JOIN reviews_photos p ON p.review_id = r.id
-    WHERE p.review_id = ${results[i].review_id} GROUP BY p.review_id`;
-
-    queries.push(getPhotosQuery);
-  }
-
-  const returnedURLs = await Promise.all(queries.map((getPhotosQuery) => db.query(getPhotosQuery)));
-  returnedURLs.map((url, i) => {
-    if (url.length > 0) {
-      results[i].photos = results[i].photos.concat(url[0].photos);
-    }
-  });
-
-  data.results = results;
-  res.send(data);
+  getReviews(req, res);
 });
 
 app.get('/reviews/meta', async (req, res) => {
